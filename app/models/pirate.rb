fb52c0865fc56767247effb02494d532063dc025
jessegan <jesse13579@gmail.com>
2020-06-30 13:31:31 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+    attr_accessor :name,:weight,:height
+    @@all = []
+
+    def initialize(opts)
+        @name=opts["name"]
+        @weight=opts["weight"]
+        @height=opts["height"]
+        @@all<<self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :name,:type,:booty
+    @@all =[]
+
+    def initialize(opts)
+        @name=opts["name"]
+        @type=opts["type"]
+        @booty=opts["booty"]
+        @@all<<self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.clear
+        @@all.clear
+    end
 end
