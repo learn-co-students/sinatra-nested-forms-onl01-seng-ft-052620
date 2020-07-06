3ee3046618826dd032210b9f8a3e7cec904d3112
sean-slaughter <sthompson2822@gmail.com>
2020-07-06 14:05:24 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+    attr_accessor :name, :weight, :height, :ships
+
+    @@all = []
+
+    def initialize(params)
+        params.each{|key, value| self.send("#{key}=", value)}
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+
 end
