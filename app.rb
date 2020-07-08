518078316b2dcf46ab277c8d30a36e796ead0364
SWFullPotential <swfullpotential@gmail.com>
2020-07-07 20:12:36 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
+    get '/' do 
+      erb :'pirates/index'
+    end
     # code other routes/actions here
+    get '/new' do 
+      erb :'pirates/new' 
+    end 
 
+    post '/pirates' do 
+      @pirate = Pirate.new(params[:pirate])
+      @ship_1 = Ship.new(params[:pirate][:ships][0])
+      @ship_2 = Ship.new(params[:pirate][:ships][1])
+      erb :"pirates/show"
+    end
   end
 end
