16e58d46f6330145deb3937865da20a09690c3b7
Elijah Kirschen <ekirschen584@gmail.com>
2020-09-04 18:23:59 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+    attr_reader :name, :weight, :height
+    @@all =[]
+
+    def initialize(args = nil)
+        @name = args["name"]
+        @weight = args["weight"]
+        @height = args["height"]
+        @@all << self
+        # binding.pry
+    end
+
+    def self.all
+        @@all
+    end
+
 end
